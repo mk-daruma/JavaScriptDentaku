40bf0afe6e796a29a8f016bf40cd4cb17a7a65de
EC2 Default User <ec2-user@ip-172-31-49-91.ec2.internal>
2022-03-02 06:36:43 +0000

    ボタンの非活性部分の追加

--- dentaku.js
+++ dentaku.js
@@ -1,12 +1,51 @@
 let display = document.getElementById("display");
+// 先頭に表示させたくないボタンのidを取得
+let Zero = document.getElementById("Zero");
+let ZeroTwo = document.getElementById("ZeroTwo");
+let equals = document.getElementById("equals");
+let plus = document.getElementById("plus");
+let minus = document.getElementById("minus");
+let multi = document.getElementById("multi");
+let divide = document.getElementById("divide");
 
+  // ディスプレイにボタンの値を表示
 function btn(button){
-  // 画面にボタンを表示させる
   display.value += button.value;
-  // 計算を実行させる、リセットさせるコード
-  if(button.value == "="){
+}
+
+
+// ＝とACのボタンの機能
+function operator(button){
+    if(button.value == "="){
     display.value = eval(display.value);
   }else if(button.value == "AC"){
     display.value = "";
   }
 }
+
+if(display.value == ""){
+  addDisabled();
+}else{
+  removeDisabled();
+}
+
+
+// ディスプレイに何も表示されていない場合、押せないようにする 
+function addDisabled(){
+    Zero.disabled = true;
+    ZeroTwo.disabled = true;
+    equals.disabled = true;
+    plus.disabled = true;
+    multi.disabled = true;
+    divide.disabled = true;
+}
+
+function removeDisabled(){
+    Zero.disabled = false;
+    ZeroTwo.disabled = false;
+    equals.disabled = false;
+    plus.disabled = false;
+    multi.disabled = false;
+    divide.disabled = false;
+}
+// 演算子を連続で押した場合、最新のものに更新されるようにする
